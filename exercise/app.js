ceb27b2d8b797065ced42fefc13b3a98154b2327
ktesibios285 <ktesibios285@gmail.com>
2023-03-22 17:56:20 -0500

    assignments

---
+++ exercise/app.js
@@ -0,0 +1,12 @@
+import {calculateCircumference} from './circleCalculator.js';
+
+function submitFunction(calculateCircumference)
+
+function updateDOM() {
+    document.getElementById("output").innerHTML = value;
+}
+
+
+parseFloat(π2r)  
+
+document.querySelector('#submitButton').onclick = submitFunction;
