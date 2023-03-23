4682a6738066dfc402afdbe2fd6ddcddefdf3b3d
ktesibios285 <ktesibios285@gmail.com>
2023-03-22 20:23:22 -0500

    exercise

---
+++ exercise/ex1-circumfrence/app.js
@@ -0,0 +1,11 @@
+import {calculateCircumference} from './circleCalculator.js';
+
+document.querySelector('#submitButton').onclick = submitFunction;
+
+function submitFunction() {
+    let radius = parseFloat(document.getElementById("radius").value);
+    let circumference = calculateCircumference(radius)
+    document.getElementById("output").innerHTML = `The circumference is ${circumference}!`;
+    return false
+}
+
