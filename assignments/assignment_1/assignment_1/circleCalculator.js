b915d1a2883e8b7a794b838a91d1c077054a628c
ktesibios285 <66027072+ktesibios285@users.noreply.github.com>
2023-03-23 15:43:45 -0500

    Add files via upload

---
+++ assignments/assignment_1/assignment_1/circleCalculator.js
@@ -0,0 +1,10 @@
+export function calculateCircumference(radius) {
+    return Math.PI * radius * 2;
+}
+export function calculateArea(radius) {
+    return Math.PI * radius * radius;
+}
+
+export function calculateSphereVolume (radius) {
+    return (4/3) * Math.PI * radius ** 3;
+}
