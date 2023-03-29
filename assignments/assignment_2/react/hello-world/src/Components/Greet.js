29f1fb79795da48806920c9586576737c5a24a7a
ktesibios285 <ktesibios285@gmail.com>
2023-03-29 06:39:54 -0500

    assignment_2

---
+++ assignments/assignment_2/react/hello-world/src/Components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello David!</h1>
+// }
+
+// JS6 arrow functions syntax
+const Greet = () => <h1>Hello David!</h1>
+
+export default Greet;
